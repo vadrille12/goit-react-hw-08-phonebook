02f3979d01e987a991928a9e3f792026f3bc58f4
vadrille <109307947+vadrille12@users.noreply.github.com>
2023-04-28 02:28:39 +0300

    delete comment

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  /* display: flex;
-  align-items: center;
-  background-color: rgb(69, 73, 78);
-  height: 70px;
-  box-shadow: 1px 5px 5px grey; */
-`;
+export const Container = styled.div``;
 
 export const Header = styled.header`
   display: flex;
